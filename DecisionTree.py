772d84d49960e223a16734796f39f01fbd2e2ecd
GithubBigHunk <128350663+Norris0333@users.noreply.github.com>
2023-09-02 10:17:45 +0630

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -11,8 +11,7 @@ response = requests.get("https://github.com/Norris0333/Simbolo_Structured_Data_P
 if response.status_code == 200:
     model_content = response.content
     # Load the trained model using pickle
-    with open('trained_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+    model = pickle.loads(model_content)
 else:
     st.error('Failed to load the model. Please check the URL.')
 

d1941fd7c50188ea602cf0eaa07b37357ed0d86f
GithubBigHunk <128350663+Norris0333@users.noreply.github.com>
2023-09-02 10:30:16 +0630

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import joblib
 from io import BytesIO
 import requests
 
 st.title('ML Prediction App')
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
-response = requests.get("https://github.com/Norris0333/Simbolo_Structured_Data_Project/blob/bd2f78eacb2bb6ed07659b49e91e4d61d6a6c5ec/train_model.pkl")
+response = requests.get("https://github.com/Norris0333/Simbolo_Structured_Data_Project/raw/main/train_model.joblib")
 
 if response.status_code == 200:
     model_content = response.content
-    # Load the trained model using pickle
-    model = pickle.loads(model_content)
+    # Save the model content to a local file
+    with open('train_model.joblib', 'wb') as model_file:
+        model_file.write(model_content)
+
+    # Load the trained model using joblib
+    model = joblib.load('train_model.joblib')
 else:
     st.error('Failed to load the model. Please check the URL.')
 

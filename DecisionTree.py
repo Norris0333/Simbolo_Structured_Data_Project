42747f219bb80845c054f44d2ccbbcc0a9cb14ea
GithubBigHunk <128350663+Norris0333@users.noreply.github.com>
2023-09-02 10:11:04 +0630

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -3,19 +3,23 @@ import pandas as pd
 import pickle
 from io import BytesIO
 import requests
-import sklearn
 
 st.title('ML Prediction App')
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 response = requests.get("https://github.com/Norris0333/Simbolo_Structured_Data_Project/blob/raw/main/trained_model.pkl")
-model_content = response.content
-# Load the trained model using pickle
-model = pickle.loads(model_content)
+
+if response.status_code == 200:
+    model_content = response.content
+    # Load the trained model using pickle
+    model = pickle.loads(model_content)
+else:
+    st.error('Failed to load the model. Please check the URL.')
+
 if uploaded_file is not None:
     # Read the uploaded .csv file
     data = pd.read_csv(uploaded_file)
-    file_X = data.iloc[:,0:-1].values
-    file_Y = data.iloc[:,-1].values
+    file_X = data.iloc[:, 0:-1].values
+    file_Y = data.iloc[:, -1].values
     predictions = model.predict(file_X)
     # Display the predictions
     st.write('Predictions:')
